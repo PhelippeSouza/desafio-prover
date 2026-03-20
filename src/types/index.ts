38934bd1ebe0ee8cf472b83aacf016838bab555b
Phelippe.Lopes <phelippesouza12@gmail.com>
2026-03-20 03:40:45 -0300

    chore: Cria arquivo de tipagem das lojas e produtos

---
+++ src/types/index.ts
@@ -0,0 +1,29 @@
+export interface Product {
+  id: string;
+  name: string;
+  price: number;
+  storeId: string;
+  description?: string;
+  quantity?: number;
+}
+
+export interface Store {
+  id: string;
+  name: string;
+  location: string;
+  phone?: string;
+  products?: Product[];
+}
+
+export interface StoreFormData {
+  name: string;
+  location: string;
+  phone?: string;
+}
+
+export interface ProductFormData {
+  name: string;
+  price: number;
+  description?: string;
+  quantity?: number;
+}
